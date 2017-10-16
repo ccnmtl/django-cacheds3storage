e43b546ca94f050cebc6ec4266910992961a1669
Nik Nyby <nnyby@columbia.edu>
2017-10-16 17:09:54 -0400

    Update author email

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="django-cacheds3storage",
     version="0.1.2",
     author="Anders Pearson",
-    author_email="anders@columbia.edu",
+    author_email="ccnmtl-dev@columbia.edu",
     url="https://github.com/ccnmtl/django-cacheds3storage",
     description="S3 cached storage",
     long_description="s3 cached storage",
