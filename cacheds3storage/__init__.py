8101408d8ad31dffcfe7956b9e0162cd297756f0
Nikolas Nyby <nikolas@gnu.org>
2020-07-16 18:54:18 -0400

    Update "seek of closed file" fix for current django-storages
    Using this class with the new django-storages gives the error:
    
      ValueError: seek of closed file
    
    This change should fix that -
    https://github.com/jschneier/django-storages/issues/382#issuecomment-654984775

--- cacheds3storage/__init__.py
+++ cacheds3storage/__init__.py
@@ -11,28 +11,31 @@ class CachedS3BotoStorage(S3Boto3Storage):
         self.local_storage = get_storage_class(
             'compressor.storage.CompressorFileStorage')()
 
-    # Fix for "ValueError: seek of closed file" problem with boto3
-    # https://github.com/jschneier/django-storages/issues/382#issuecomment-377174808
-    def _save_content(self, obj, content, parameters):
+    # https://github.com/jschneier/django-storages/issues/382
+    #    #issuecomment-592876060
+    def _save(self, name, content):
         """
-        We create a clone of the content file as when this is passed to boto3 it wrongly closes
-        the file upon upload where as the storage backend expects it to still be open
+        We create a clone of the content file as when this is passed
+        to boto3 it wrongly closes the file upon upload where as the
+        storage backend expects it to still be open
         """
         # Seek our content back to the start
         content.seek(0, os.SEEK_SET)
 
-        # Create a temporary file that will write to disk after a specified size
-        content_autoclose = SpooledTemporaryFile()
+        # Create a temporary file that will write to disk after a
+        # specified size. This file will be automatically deleted when
+        # closed by boto3 or after exiting the `with` statement if the
+        # boto3 is fixed
+        with SpooledTemporaryFile() as content_autoclose:
 
-        # Write our original content into our copy that will be closed by boto3
-        content_autoclose.write(content.read())
+            # Write our original content into our copy that will be
+            # closed by boto3
+            content_autoclose.write(content.read())
 
-        # Upload the object which will auto close the content_autoclose instance
-        super(CachedS3BotoStorage, self)._save_content(obj, content_autoclose, parameters)
-
-        # Cleanup if this is fixed upstream our duplicate should always close
-        if not content_autoclose.closed:
-            content_autoclose.close()
+            # Upload the object which will auto close the
+            # content_autoclose instance
+            return super(CachedS3BotoStorage, self)._save(
+                name, content_autoclose)
 
     def save(self, name, content):
         name = super(CachedS3BotoStorage, self).save(name, content)
